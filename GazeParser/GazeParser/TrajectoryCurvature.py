744a3bf0d4d4430ac51ac7eb8cad2a800ddcc88e
Hiroyuki Sogo <hsogo515@gmail.com>
2012-12-07 14:24:43 +0900

    MOD: getAreaCurvature supports ignoreNaN option, getRotatedTrajectory supports trajectory data, parameter check in some methods.

--- GazeParser/GazeParser/TrajectoryCurvature.py
+++ GazeParser/GazeParser/TrajectoryCurvature.py
@@ -8,48 +8,71 @@
 
 import numpy
 
-def getAreaCurvature(traj,absolute=False):
+def getAreaCurvature(traj,absolute=False,ignoreNaN=False):
     """
-    Get are curvature of a trajectory. The area curvature is defined as the
+    Get area curvature of a trajectory. The area curvature is defined as the
     area surrounded by the X axis and the tracjectory.  The origin and
     direction of the trajectory must be normalized in advance.
     
-    :param traj: Nomalized trajectory.
-    :param absolute: If False, the area below the X axis has a negative value.
+    :param numpy.ndarray traj:
+        Nomalized trajectory (binocular data is not currently supported).
+    :param bool absolute:
+        If False, the area below the X axis has a negative value.
         Default value is False.
+    :param bool ignoreNone:
+        If True, NaNs in the trajectory are removed before calculating
+        area curvature. Default value is False.
     """
+    if traj.shape[1] != 2:
+        raise ValueError, 'size of the trajectory must be n x 2.'
+    
     if absolute:
         y = abs(traj[:,1])
     else:
         y = traj[:,1]
-    return numpy.trapz(y,traj[:,0])
+    x = traj[:,0]
+    
+    if ignoreNaN:
+        idx = numpy.isnan(y)==False
+        y = y[idx]
+        x = x[idx]
+    
+    return numpy.trapz(y,x)
 
 def getInitialDirection(traj,index,unit='rad'):
     """
     Get the initila direction of the trajectory.
     The origin and direction of the trajectory must be normalized in advance.
     
-    :param traj: Nomalized trajectory.
-    :param index: Specify which sample is used to calculate direction.
+    :param numpy.ndarray traj:
+        Nomalized trajectory (binocular data is not currently supported).
+    :param int index: Specify which sample is used to calculate direction.
         For example, the third sample is used if this parameter is 2.
         Note that the index of the origin is zero.
     :param unit: Specify unit of the returned value ('deg' or 'rad').
     """
+    if traj.shape[1] != 2:
+        raise ValueError, 'size of the trajectory must be n x 2.'
+    
     r = numpy.arctan2(traj[index,1],traj[index,0])
     if unit.lower() == 'rad':
         return r
     elif unit.lower() == 'deg':
         return r*180.0/numpy.pi
     else:
-        raise 'unit must be \'rad\' or \'deg\''
+        raise ValueError, 'unit must be \'rad\' or \'deg\''
 
 def getMaximumDeviation(traj):
     """
     Get maximum deviation of trajectory from the X axis.
     The origin and direction of the trajectory must be normalized in advance.
     
-    :param traj: Nomalized trajectory.
+    :param numpy.ndarray traj:
+        Nomalized trajectory (binocular data is not currently supported).
     """
+    if traj.shape[1] != 2:
+        raise ValueError, 'size of the trajectory must be n x 2.'
+    
     absY = numpy.abs(traj[:,1])
     idx = numpy.where(absY == numpy.max(absY))[0][0]
     return traj[idx,1]
@@ -85,13 +108,14 @@ def getRotatedTrajectory(sac,refPoint=None,refPointRelative=None,rot=None,unit='
     the same to that of the original trajectory, set 'original' to the 
     parameter 'origin'.
     
-    :param sac: An instance of :class:`~GazeParser.Core.SaccadeData`.
+    :param sac: An instance of :class:`~GazeParser.Core.SaccadeData` or an 
+        an array of saccade trajectory.
     :param refPoint: The reference point for rotation. See above for detail.
     :param refPointRelative: The reference point for rotation. See above for 
         detail.
-    :param rot: The angle of rotation.
+    :param float rot: The angle of rotation.
     :param unit: Specify unit of the rotation angle ('deg' or 'rad').
-    :param normalize: If this value is not zero, the distance between the start
+    :param float normalize: If this value is not zero, the distance between the start
         and the end point of the trajectory is normalized to the given value.
         If zero, the distance is the same to that of the original trajectory.
         Default value is zero.
@@ -102,7 +126,7 @@ def getRotatedTrajectory(sac,refPoint=None,refPointRelative=None,rot=None,unit='
         Note that this parameter does NOT specify the center of rotation.
     """
     if [refPoint,refPointRelative,rot].count(None) < 2:
-        raise 'Only one of refPoint, refPointRelative, rot can be specified.'
+        raise ValueError, 'Only one of refPoint, refPointRelative, rot can be specified.'
         return
         
     if refPoint != None:
@@ -114,9 +138,17 @@ def getRotatedTrajectory(sac,refPoint=None,refPointRelative=None,rot=None,unit='
     else:
         mode = 'ScreenCoordinate'
     
-    sp = sac.start
-    e = sac.end
-    traj = sac.getTraj() - sp
+    if isinstance(sac,GazeParser.Core.SaccadeData):
+        if sac._parent._recordedEye=='B':
+            raise ValueError, 'Binocular data is nor currently supported. Get left or right eye\' trajectory using getTraj().'
+        sp = sac.start
+        e = sac.end
+        traj = sac.getTraj() - sp
+    else:
+        sp = sac[0,:]
+        e = sac[-1,:]
+        traj = sac-sp
+    
     if mode=='RefPoint':
         rot = -numpy.arctan2(e[1]-sp[1],e[0]-sp[0])
         rot += numpy.arctan2(refPoint[1]-sp[1],refPoint[0]-sp[0])
@@ -129,9 +161,9 @@ def getRotatedTrajectory(sac,refPoint=None,refPointRelative=None,rot=None,unit='
         if unit.lower()=='deg':
             rot = numpy.pi / 180.0 * rot
         else:
-            raise 'unit must be \'rad\' or \'deg\''
+            raise ValueError, 'unit must be \'rad\' or \'deg\''
     else: #ScreenCoordinate
-        rot = -numpy.arctan2(e[1]-sp[1],e[0]-sp[0])
+        rot = -numpy.arctan2(e[1]-  sp[1],e[0]-sp[0])
     
     rotmat = numpy.array([[numpy.cos(rot),-numpy.sin(rot)],[numpy.sin(rot),numpy.cos(rot)]])
     rottraj = numpy.zeros(traj.shape)
