dd0a80050a2ee548cf9d1f02a924a5f81e07589a
Hiroyuki Sogo <hsogo12600@gmail.com>
2014-08-11 19:31:39 +0900

    ADD: version-number check tool

---
+++ VersionNumTest.py
@@ -0,0 +1,20 @@
+import re
+
+s = re.compile('\d+\.\d+\.\d')
+files = ['GazeParser/GazeParser/__init__.py',
+         'GazeParser/Readme.rst',
+         'GazeParser/GazeParser/app/Viewer.cfg',
+         'GazeParser/debian/changelog',
+         'SimpleGazeTracker/configure.in',
+         'SimpleGazeTracker/NEWS',
+         'SimpleGazeTracker/common/GazeTrackerCommon.h',
+         'SimpleGazeTracker/debian/changelog',
+         ]
+
+for file in files:
+    print file
+    fp = open(file,'r')
+    for line in fp:
+        m =s.search(line)
+        if m != None:
+            print m.group(0)
