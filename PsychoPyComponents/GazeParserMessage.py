89d979ae8710f8d316cc5a3653b14825e1a6d42a
Hiroyuki Sogo <hsogo12600@gmail.com>
2015-03-07 15:19:10 +0900

    ENH: 'condition' is supported by GazeParserMessage component.

--- PsychoPyComponents/GazeParserMessage.py
+++ PsychoPyComponents/GazeParserMessage.py
@@ -1,51 +1,70 @@
-from _base import * #to get the template visual component
+"""
+.. Part of GazeParser package.
+.. Copyright (C) 2012-2015 Hiroyuki Sogo.
+.. Distributed under the terms of the GNU General Public License (GPL).
+"""
+
+from _base import *
 from os import path
-from psychopy.app.builder.experiment import Param
+
+# only use _localized values for label values, nothing functional
+_localized = {'name': _translate('Name'),  # fieldName: display label
+              'startType': _translate('start type'), 'stopType': _translate('stop type'),
+              'startVal': _translate('Start'), 'stopVal': _translate('Stop'),
+              'startEstim': _translate('Expected start (s)'), 'durationEstim': _translate('Expected duration (s)')
+             }
 
 thisFolder = path.abspath(path.dirname(__file__))#the absolute path to the folder containing this path
 iconFile = path.join(thisFolder,'GazeParserMessage.png')
 tooltip = 'GazeParserMessage: sending a message to SimpleGazeTracker'
 
 # want a complete, ordered list of codeParams in Builder._BaseParamsDlg, best to define once:
-paramNames = ['time','text','units']
+paramNames = ['time','timeType','text']
 
 class GazeParserMessageComponent(BaseComponent):
     """Recording with GazeParser.TrackingTools"""
     categories = ['Custom']
-    def __init__(self, exp, parentName, name='GazeParserMessage', time=0, text='message', units='time (s)'):
+    def __init__(self, exp, parentName, name='GazeParserMessage', timeType='time (s)', time=0.0, text='message'):
         self.type='GazeParserMessage'
         self.url="http://gazeparser.sourceforge.net/"
-        self.exp=exp#so we can access the experiment if necess
+        super(GazeParserMessageComponent, self).__init__(exp, parentName, name)
+
         #params
-        self.categories=['misc']
         self.order = ['name'] + paramNames[:] # want a copy, else codeParamNames list gets mutated
-        self.params={}
-        self.params['name']=Param(name, valType='code', allowedTypes=[],
-            hint="",
-            label="Name")
         self.params['time']=Param(time, valType='code', allowedTypes=[],
-            hint="Message time",
-            label="Time")
-        self.params['text']=Param(text, valType='str', allowedTypes=[],
+            updates='constant', allowedUpdates=['constant','set every repeat','set every frame'],
+            hint=_translate("When does the component start?"),
+            label=_localized['startVal'])
+        self.params['timeType']=Param(text, valType='str', allowedVals=['time (s)', 'frame N', 'condition'],
             updates='constant', allowedUpdates=[],
+            hint=_translate("How do you want to define your start point?"),
+            label=_localized['startType'])
+        self.params['text']=Param(text, valType='str', allowedTypes=[],
+            updates='constant', allowedUpdates=['constant','set every repeat','set every frame'],
             hint="Message text",
             label="Message text")
-        self.params['units']=Param(units, valType='str', allowedVals=['frame N', 'time (s)'],
-            hint="Units of message sending time",
-            label="Unit of time")
+
+        # these inherited params are harmless but might as well trim:
+        for p in ['startType', 'startVal', 'startEstim', 'stopVal', 'stopType', 'durationEstim']:
+            del self.params[p]
 
     def writeRoutineStartCode(self,buff):
         buff.writeIndented('%(name)s_sent=False\n' % (self.params))
 
     def writeFrameCode(self,buff):
-        if self.params['units'].val=='time (s)':
+        if self.params['startType'].val=='time (s)':
             buff.writeIndented('if %(name)s_sent==False and %(time)s<=t:\n' % (self.params))
             buff.setIndentLevel(+1, relative=True)
             buff.writeIndented('GazeParserTracker.sendMessage(%(text)s)\n' % (self.params))
             buff.writeIndented('%(name)s_sent=True\n' % (self.params))
             buff.setIndentLevel(-1, relative=True)
-        else: # frame N
+        elif self.params['startType'].val=='frame N':
             buff.writeIndented('if %(time)s==frameN:\n' % (self.params))
+            buff.setIndentLevel(+1, relative=True)
+            buff.writeIndented('GazeParserTracker.sendMessage(%(text)s)\n' % (self.params))
+            buff.setIndentLevel(-1, relative=True)
+        else: # condition
+            buff.writeIndented('if %(time)s:\n' % (self.params))
             buff.setIndentLevel(+1, relative=True)
             buff.writeIndented('GazeParserTracker.sendMessage(%(text)s)\n' % (self.params))
             buff.setIndentLevel(-1, relative=True)
