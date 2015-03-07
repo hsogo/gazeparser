31634ce5da22a37d4c19b7c9c7efc4c4e9b78df5
Hiroyuki Sogo <hsogo12600@gmail.com>
2015-03-07 15:19:53 +0900

    ENH: Dummy mode can be set using variable

--- PsychoPyComponents/GazeParserInit.py
+++ PsychoPyComponents/GazeParserInit.py
@@ -1,32 +1,32 @@
+"""
+.. Part of GazeParser package.
+.. Copyright (C) 2012-2015 Hiroyuki Sogo.
+.. Distributed under the terms of the GNU General Public License (GPL).
+"""
 
-from _base import *
+from _visual import VisualComponent, Param
 from os import path
-from psychopy.app.builder.experiment import Param
 
 thisFolder = path.abspath(path.dirname(__file__))#the absolute path to the folder containing this path
 iconFile = path.join(thisFolder,'GazeParserInit.png')
 tooltip = 'GazeParserInit: initialize GazeParser.TrackingTools'
 
 # want a complete, ordered list of codeParams in Builder._BaseParamsDlg, best to define once:
-paramNames = ['gpconfigfile', 'trconfigfile', 'ipaddress', 'calarea', 'caltargetpos', 'datafile', 'dummymode', 'units', 'calibration']
+paramNames = ['gpconfigfile', 'trconfigfile', 'ipaddress', 'calarea', 'caltargetpos', 'datafile', 'calibration', 'units', 'mode', 'modevar']
 
 
-class GazeParserInitComponent(BaseComponent):
+class GazeParserInitComponent(VisualComponent):
     """Initialize GazeParser.TrackingTools"""
     categories = ['Custom']
     def __init__(self, exp, parentName, name='GazeParserInit',gpconfigfile="",trconfigfile="",ipaddress="",calarea="[-400,-300,400,300]",
                  caltargetpos="[[0,0],[-350,-250],[-350,0],[-350,250],\n[0,-250],[0,0],[0,250],\n[350,-250],[350,0],[350,250]]",
-                 datafile="data.csv",dummymode=False,units="pix",calibration=True):
+                 datafile="data.csv",mode='Normal',modevar='',units="pix",calibration=True):
+        super(GazeParserInitComponent, self).__init__(exp, parentName, name)
         self.type='GazeParserInit'
         self.url="http://gazeparser.sourceforge.net/"
-        self.exp=exp#so we can access the experiment if necess
+
         #params
-        self.categories=['misc']
         self.order = ['name'] + paramNames[:] # want a copy, else codeParamNames list gets mutated
-        self.params={}
-        self.params['name']=Param(name, valType='code', allowedTypes=[],
-            hint="",
-            label="Name")
         self.params['gpconfigfile']=Param(ipaddress, valType='str', allowedTypes=[],
             updates='constant', allowedUpdates=[],
             hint="GazeParser configuration file",
@@ -55,21 +55,34 @@ class GazeParserInitComponent(BaseComponent):
             updates='constant', allowedUpdates=[],
             hint="Name of SimpleGazeTracker data file.",
             label="SimpleGazeTracker Data File")
-        self.params['dummymode']=Param(dummymode, valType='bool', allowedTypes=[],
+        self.params['mode']=Param(mode, valType='str', allowedVals=['Normal','Dummy','Follow the variable'],
             updates='constant', allowedUpdates=[],
-            hint="Dummy mode",
-            label="Dummy Mode")
-        self.params['units']=Param(units, valType='str', allowedVals=['deg', 'cm', 'pix', 'norm'],
-            hint="Units for calibration area and calibration target positions",
-            label="Units")
+            hint="Choose 'Dummy' to force dummy mode. If you want to choose mode at run time,\nchoose 'Follow the variable' and set name of the variable to 'Mode Variable'",
+            label="Mode", categ="Advanced")
+        self.params['modevar']=Param(modevar, valType='code', allowedTypes=[],
+            updates='constant', allowedUpdates=[],
+            hint="Set name of the variable. This parameter is effective only if 'Mode' is 'Follow the variable'\nThe experiment run in dummy mode if value of the variable is True. Otherwise, run in normal mode.",
+            label="Mode Variable", categ="Advanced")
+        # these inherited params are harmless but might as well trim:
+        for p in ['startType', 'startVal', 'startEstim', 'stopVal', 'stopType', 'durationEstim']:
+            del self.params[p]
+        for p in ['color','opacity','colorSpace','pos','size','ori']:
+            del self.params[p]
     def writeInitCode(self,buff):
+        if self.params['mode'].val == 'Normal':
+            isDummyMode = False
+        elif self.params['mode'].val == 'Dummy':
+            isDummyMode = True
+        else: # Follow the variable
+            isDummyMode = self.params['modevar'].val
         buff.writeIndented('import GazeParser\nimport GazeParser.Configuration\nimport GazeParser.TrackingTools\n')
         if self.params['gpconfigfile'].val != '':
             buff.writeIndented('GazeParser.config=GazeParser.Configuration.Config(%(gpconfigfile)s)\n' % (self.params))
-        if self.params['trconfigfile'].val == '':
-            buff.writeIndented('GazeParserTracker = GazeParser.TrackingTools.getController(backend="PsychoPy",dummy=%(dummymode)s)\n' % (self.params))
-        else:
-            buff.writeIndented('GazeParserTracker = GazeParser.TrackingTools.getController(backend="PsychoPy",configFile=%(trconfigfile)s,dummy=%(dummymode)s)\n' % (self.params))
+        getControllerCommand = 'GazeParserTracker = GazeParser.TrackingTools.getController(backend="PsychoPy", '
+        if self.params['trconfigfile'].val != '':
+            getControllerCommand += 'configFile=%(trconfigfile)s, ' % (self.params)
+        getControllerCommand += 'dummy=%s)\n' % (isDummyMode)
+        buff.writeIndented(getControllerCommand)
         buff.writeIndented('GazeParserTracker.connect(%(ipaddress)s)\n' % (self.params))
         if self.params['datafile'].val != '':
             buff.writeIndented('GazeParserTracker.openDataFile(%(datafile)s)\n' % (self.params))
@@ -88,6 +101,8 @@ class GazeParserInitComponent(BaseComponent):
             buff.setIndentLevel(+1, relative=True)
             buff.writeIndented('break\n\n')
             buff.setIndentLevel(-2, relative=True)
+    def writeFrameCode(self,buff):
+        pass
     def writeExperimentEndCode(self,buff):
         if self.params['datafile'].val != '':
             buff.writeIndented('GazeParserTracker.closeDataFile()\n')
