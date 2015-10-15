511bc23493eb5230129b1d0cb6f309cc4e1caba7
Hiroyuki Sogo <hsogo12600@gmail.com>
2015-10-15 21:00:15 +0900

    MOD: update build_installer.bat

--- GazeParser/build_installer.bat
+++ GazeParser/build_installer.bat
@@ -1,4 +1,3 @@
-.\setup.py clean --all
-.\setup.py sdist
-.\setup.py build
-.\setup.py bdist_wininst --install-script GazeParser_post_install.py --user-access-control force
+python setup.py clean --all
+python setup.py sdist
+python setup.py build bdist_wininst --install-script GazeParser_post_install.py
