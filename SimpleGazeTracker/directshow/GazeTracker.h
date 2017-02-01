9165a841e499aa5bf5da8270fb90dfba5e2a1feb
Hiroyuki Sogo <hsogo12600@gmail.com>
2017-02-01 13:56:28 +0900

    ADD: add DirectShow edition

---
+++ SimpleGazeTracker/directshow/GazeTracker.h
@@ -0,0 +1,14 @@
+/*!
+@file GazeTracker.h
+@author Hiroyuki Sogo
+@copyright GNU General Public License (GPL) version 3.
+@brief Camera-dependent constants are defined.
+
+@date 2017/01/17
+- Created.
+*/
+
+#define EDITION "DirectShow Edition"
+#define CAMERA_CONFIG_FILE "CONFIG_DIRECTSHOW"
+
+#include "../common/GazeTrackerCommon.h"
