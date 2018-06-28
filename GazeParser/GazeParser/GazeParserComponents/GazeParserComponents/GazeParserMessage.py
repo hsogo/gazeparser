3240feb3281da2a1652542da0fd6333a5f7f7a63
Hiroyuki Sogo <hsogo12600@gmail.com>
2018-06-28 21:12:36 +0900

    MOD: remove components for PsychoPy 1.83

--- GazeParser/GazeParser/GazeParserComponents/GazeParserComponents/GazeParserMessage.py
+++ GazeParser/GazeParser/GazeParserComponents/GazeParserComponents/GazeParserMessage.py
@@ -3,6 +3,10 @@
 .. Copyright (C) 2012-2016 Hiroyuki Sogo.
 .. Distributed under the terms of the GNU General Public License (GPL).
 """
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 
 try:
     from psychopy.experiment.components import *
