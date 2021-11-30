42a563a113164cc7acabe8d4be413e241f2b46f2
Hiroyuki Sogo <hsogo12600@gmail.com>
2021-11-30 12:18:59 +0900

    MOD: add start_rec & stop_rec parameter to RecComponent

--- GazeParser/GazeParser/GazeParserComponents/GazeParserComponents/GazeParserRec.py
+++ GazeParser/GazeParser/GazeParserComponents/GazeParserComponents/GazeParserRec.py
@@ -15,7 +15,7 @@ except:
 from os import path
 
 # want a complete, ordered list of codeParams in Builder._BaseParamsDlg, best to define once:
-paramNames = ['startmsg', 'stopmsg']
+paramNames = ['startrec', 'startmsg', 'stoprec', 'stopmsg']
 
 class GazeParserRecComponent(BaseComponent):
     """Recording with GazeParser.TrackingTools"""
@@ -24,7 +24,7 @@ class GazeParserRecComponent(BaseComponent):
     iconFile = path.join(thisFolder,'GazeParserRec.png')
     tooltip = 'GazeParserRec: recording with GazeParser.TrackingTools'
 
-    def __init__(self, exp, parentName, name='GazeParserRec', startmsg='routine start', stopmsg='routine end'):
+    def __init__(self, exp, parentName, name='GazeParserRec', startrec=True, stoprec=True, startmsg='routine start', stopmsg='routine end'):
         super(GazeParserRecComponent, self).__init__(exp, parentName, name)
         
         self.type='GazeParserRec'
@@ -33,10 +33,18 @@ class GazeParserRecComponent(BaseComponent):
         
         #params
         self.order = ['name'] + paramNames[:] # want a copy, else codeParamNames list gets mutated
+        self.params['startrec']=Param(startrec, valType='bool', allowedTypes=[],
+            updates='constant', allowedUpdates=[],
+            hint="Start recording at the beginning of this routine.  Uncheck this if recording is continuing from the preceding routine.",
+            label="Start Recording")
         self.params['startmsg']=Param(startmsg, valType='str', allowedTypes=[],
             updates='constant', allowedUpdates=['constant','set every repeat'],
             hint="The message sent when recording is started.",
             label="Message (Start)")
+        self.params['stoprec']=Param(stoprec, valType='bool', allowedTypes=[],
+            updates='constant', allowedUpdates=[],
+            hint="Stop recording at the end of this routine.  Uncheck this if you want to continue recording in the next routine.",
+            label="Stop Recording")
         self.params['stopmsg']=Param(stopmsg, valType='str', allowedTypes=[],
             updates='constant', allowedUpdates=['constant','set every repeat'],
             hint="The message sent when recording is stopped.",
@@ -45,13 +53,29 @@ class GazeParserRecComponent(BaseComponent):
         for p in ['startType', 'startVal', 'startEstim', 'stopVal', 'stopType', 'durationEstim']:
             del self.params[p]
 
+        self.depends += [  # allows params to turn each other off/on
+            {"dependsOn": "startrec",  # must be param name
+             "condition": "== True",  # val to check for
+             "param": "startmsg",  # param property to alter
+             "true": "enable",  # what to do with param if condition is True
+             "false": "disable",  # permitted: hide, show, enable, disable
+             },
+            {"dependsOn": "stoprec",  # must be param name
+             "condition": "== True",  # val to check for
+             "param": "stopmsg",  # param property to alter
+             "true": "enable",  # what to do with param if condition is True
+             "false": "disable",  # permitted: hide, show, enable, disable
+             }
+        ]
     def writeRoutineStartCode(self,buff):
-        buff.writeIndented('GazeParserTracker.startRecording(%(startmsg)s)\n' % (self.params))
-        buff.writeIndented('routineTimer.add('+self.parentName+'Clock.getTime())\n')
-        buff.writeIndented(self.parentName+'Clock.reset() # clock must be reset because startRecording() takes hundreds of milliseconds\n')
-        buff.writeIndented('GazeParserTracker.sendMessage(\'rec_sync\')\n' % (self.params))
-        buff.writeIndented('logging.exp(\'GazeParser rec_sync\')\n' % (self.params))
+        if self.params['startrec'].val:
+            buff.writeIndented('GazeParserTracker.startRecording(%(startmsg)s)\n' % (self.params))
+            buff.writeIndented('routineTimer.add('+self.parentName+'Clock.getTime())\n')
+            buff.writeIndented(self.parentName+'Clock.reset() # clock must be reset because startRecording() takes hundreds of milliseconds\n')
+            buff.writeIndented('GazeParserTracker.sendMessage(\'rec_sync\')\n' % (self.params))
+            buff.writeIndented('logging.exp(\'GazeParser rec_sync\')\n' % (self.params))
 
     def writeRoutineEndCode(self,buff):
-        buff.writeIndented('GazeParserTracker.stopRecording(%(stopmsg)s)\n' % (self.params))
+        if self.params['stoprec'].val:
+            buff.writeIndented('GazeParserTracker.stopRecording(%(stopmsg)s)\n' % (self.params))
         
