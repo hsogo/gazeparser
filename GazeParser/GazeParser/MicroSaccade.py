0b30589945b7d80430f52add44790e6b5e3a43da
Hiroyuki Sogo <hsogo12600@gmail.com>
2025-02-14 13:42:34 +0900

    MOD: update copyright

--- GazeParser/GazeParser/MicroSaccade.py
+++ GazeParser/GazeParser/MicroSaccade.py
@@ -1,6 +1,6 @@
 """
 .. Part of GazeParser package.
-.. Copyright (C) 2012-2015 Hiroyuki Sogo.
+.. Copyright (C) 2012-2023 Hiroyuki Sogo.
 .. Distributed under the terms of the GNU General Public License (GPL).
 
 Detecting microsaccades with the method of Engbert & Kliegl (2003).
